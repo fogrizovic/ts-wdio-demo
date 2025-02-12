aca6c04512a9fc564e1ada9e343e8dbab811c651
Federico Ogrizovic <fogrizovic@gmail.com>
2025-02-12 00:25:22 -0300

    add ci

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -52,7 +52,12 @@ export const config: WebdriverIO.Config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: process.env.CI === 'true'
+                ? ['--headless', '--disable-gpu', '--no-sandbox']  // Headless for CI mode
+                : []  // Non-headless for local mode
+        }
     }],
 
     //
@@ -102,7 +107,7 @@ export const config: WebdriverIO.Config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: ['selenium-standalone'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
